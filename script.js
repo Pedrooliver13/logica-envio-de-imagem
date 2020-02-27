b42c82080378e0064b4efd91ce79eb0d9f4c1478
Pedro Oliveira <56042296+Pedrooliver13@users.noreply.github.com>
2020-02-27 17:55:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,6 +11,24 @@ const PhotosUpload = {
       return
     }
     
-    
+    //pegando fotos(image) com input e adicionamos a uma div que vai entrar em outra div
+    Array.from(fileList).forEach(file =>{
+      const reader = new FileReader()
+      
+      reader.onload = ()=>{//onload == espera ficar pronto
+        const image = new Image()
+        image.src = String(reader.results)
+        
+        const div = document.CreatedElement('div')
+        div.classList.add('photos')
+        
+        div.onclick = ()=> alert('Aqui lógica de deletar (ainda em produção)')
+        
+        div.appendChild(image)
+        documen.querySelector('#photos-preview').appendChild(div)
+      }
+      
+      reader.readAsDataUrl(file)//ele avisa quando está pronto
+    })
   }
 }
